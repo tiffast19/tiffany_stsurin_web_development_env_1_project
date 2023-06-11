4de1b5cbe126e4f391bc1d1fe46dfbd35ff6185d
tiffast19 <134743302+tiffast19@users.noreply.github.com>
2023-06-11 16:07:37 -0400

    content test

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home Page - Tiffany St-Surin</title>
+	<link rel="stylesheet" href="asset/css/about.css">
+</head>
+<body>
+<header class="page-header">
+		<div class="container">
+			<div class="header-flex">
+				<div class="logo">
+					<a href="index.html">
+						<h2>TS</h2>
+					</a>
+				</div>
+
+				<div class="andres">
+					<i class="fa-solid fa-bars"></i>
+				</div>
+
+				<nav>
+					<ul class="header-menu">
+						<li>
+							<a href="index.html">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						
+						<li>
+							<a href="">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+	<section class="about">
+		<div class="container">
+			<div class="about-flex">
+				<div class="left"></div>
+				<div class="right">
+					<h3>Tiffany St-Surin</h3>
+					<hr>
+					<p></p><br>
+					
+					<a href="" class="btn">learn more</a>				
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="cta">
+		<h3>Contact me</h3>
+	</section>
+
+	
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Final Project - Tiffany St-Surin</title>
-    
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home Page - Tiffany St-Surin</title>
+	<link rel="stylesheet" href="asset/css/index.css">
 </head>
 <body>
-    
+<header class="page-header">
+		<div class="container">
+			<div class="header-flex">
+				<div class="logo">
+					<a href="index.html">
+						<h2>TS</h2>
+					</a>
+				</div>
+
+				<div class="andres">
+					<i class="fa-solid fa-bars"></i>
+				</div>
+
+				<nav>
+					<ul class="header-menu">
+						<li>
+							<a href="index.html">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						
+						<li>
+							<a href="">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+	<section class="big-banner">
+		<div class="textbox">
+			<h1>Tiffany St-Surin</h1>
+			<p>Aspiring Web Developper</p>
+		</div>
+	</section>
+
+	<section class="cta">
+		<h3>Contact me</h3>
+	</section>
+
+	
 </body>
 </html>
